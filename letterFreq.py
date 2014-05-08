2050f547a5e2fec414f898d0ac85ef892bd6e52b
axellarsstenson <axellarsstenson@gmail.com>
2014-05-08 15:24:46 -0500

    Create letterFreq.py

---
+++ letterFreq.py
@@ -0,0 +1,18 @@
+
+
+
+f = open('bigText.txt', 'r')
+
+searchString = f.read()
+
+alphaList = { 'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0, 'n' : 0,
+              'o' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+
+for i in searchString:
+    if (i.isalpha() == True):
+        lowerLet = i.lower()
+        alphaList[lowerLet] +=  1
+
+print(alphaList)
+
+f.close()
